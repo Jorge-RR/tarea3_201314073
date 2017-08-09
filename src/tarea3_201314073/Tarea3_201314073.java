2f13d092a69a22dc11077357b5159cf150468067
MAX SANKRIS <MAX SANKRIS@Jorge.Rodriguez>
2017-08-09 17:43:30 -0600

    tarea3_201314073-2

--- src/tarea3_201314073/Tarea3_201314073.java
+++ src/tarea3_201314073/Tarea3_201314073.java
@@ -5,17 +5,88 @@
  */
 package tarea3_201314073;
 
+import java.util.Scanner;
+
 /**
  *
- * @author MAX SANKRIS
+
  */
 public class Tarea3_201314073 {
+
+   
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Tarea_3 ");
+        System.out.println("201314073");
+
+int menprincipal=0; 
+    Scanner lector = new Scanner (System.in);
+        System.out.println("1. usuario");
+        System.out.println("2. palabras palíndromas");
+        System.out.println("3.salir");
+           menprincipal=lector.nextInt();
+
+switch(menprincipal){
+ 
+    case 1: 
+             int elegir=0;
+             System.out.println("Menu usuario");
+             Scanner opciones = new Scanner(System.in);
+             System.out.println("1. ingressar usuario");
+             System.out.println("2. Mostrar todo los usuarios");
+             System.out.println("3. Mostrar un usuario personalizado");
+             System.out.println("4. menu principal");
+             System.out.println("5. salir");
+               elegir=opciones.nextInt();
+                
+                if(elegir==1){
+                    
+                          
+                       
+                      String contenido;
+                          String []cont= new String[5];
+                          for(int i=0; i< cont.length;i++){
+                           Scanner lector1= new Scanner(System.in);
+                          System.out.println("ingrese un numero");
+                                contenido=lector1.next();
+                            cont[0]=contenido;
+                           
+            
+                        }
+                 
+               
+                }
+                if(elegir==2){
+                
+                }
+                if(elegir==3){}
+                if (elegir==4){}
+                if(elegir==5){}
+               
+               
+
+    break;
+     case 2:
+            System.out.println("Palabras palindromas");
+            String palabra;
+            Scanner elegir1 = new Scanner(System.in);
+            System.out.println("Ingrese Palabra");
+            palabra=elegir1.toString();
+            
+            
+                     break;
+     case 3:
+                     break;                 
+default: 
+                System.out.println("opcion incorrecta");
+            break;
+}
+
+// TODO code application logic here
     }
     
 }
